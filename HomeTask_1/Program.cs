5632fbeba3a2e292abd4f92d49021dc7348e9c60
ArtemKuznetsov44 <kuzia.artem456485@mail.ru>
2021-08-30 00:40:43 +0300

    first homework

---
+++ HomeTask_1/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace HomeTask_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Здравствуй ! Пожалуйста, введи свое имя: ");
+            string name = Console.ReadLine();
+            Console.WriteLine($"Привет, {name}, сегодня {DateTime.Now}");
+            Console.Read(); 
+        }
+    }
+}
