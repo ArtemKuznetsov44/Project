82d92ae7be94c1def02601d99699f7ae6cc79b2b
ArtemKuznetsov44 <kuzia.artem456485@mail.ru>
2021-09-02 23:28:51 +0300

    Lesson 3.HomeWork

---
+++ HomeTask_3 (№1)/Program.cs
@@ -0,0 +1,82 @@
+﻿using System;
+
+/*
+ * Написать программу, выводящую элементы двухмерного массива по диагонали.
+ */
+
+namespace HomeTask_3___1_
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите размерности двумерного массива:");
+
+            // Получаем кол-во строк для нашего массива.
+            Console.Write("Кол-во строк: "); 
+            int rows = int.Parse(Console.ReadLine());
+
+            // Получаем кол-во столбцов для нашего массива.
+            Console.Write("Кол-во столбцов: "); 
+            int columns = int.Parse(Console.ReadLine());
+
+            // Создаем двумерный целочисленный массив.
+            int[,] array = new int[rows, columns];
+            
+            // Вызываем функцию, которая будет заполнять наш массив случаныйми числами.
+            Zap(array, rows, columns); 
+
+            // Вызываем функцию, которая будет выводить все элементы двумерного массива по диагонали.
+            Print(array, rows, columns);
+
+            Console.Read(); 
+            
+        }
+        static void Print(int[,] array, int rows, int columns)
+        {
+            string str = ""; 
+            for(int i = 0; i < rows; i++)
+            {
+                for (int j = 0; j < columns; j++)
+                {
+                    Console.WriteLine(str + array[i, j].ToString());
+                    str += ">"; // В качестве наглядности, вместо пробела в данном выводе установил знак "больше".
+                }
+            }
+            Console.WriteLine();
+            Console.WriteLine("А теперь в другую сторону:");
+            int SIZE = array.Length; 
+            for (int i = 0; i < rows; i++)
+            {
+                for (int j = 0; j < columns; j++)
+                {
+                    string space = "";
+                    while (space.Length != SIZE - 1 )
+                    {
+                        space += "<"; // В качестве наглядности,
+                                      // вместо пробела в данном выводе установил знак "меньше".
+                    }
+                    SIZE -= 1;
+                    Console.WriteLine(space + array[i, j].ToString()); 
+                }
+            }
+        }
+        static int[,] Zap(int[,] array, int rows, int columns)
+        {
+            // Класс Random служит для генерации случайных чисел.
+            // Создадим экземпляр данного класса.
+            Random rnd = new Random();
+
+            for (int i = 0; i < rows; i++)
+            {
+                for (int j = 0; j < columns; j++)
+                {
+                    // В каждую "ячейку" добавляется случаной сгенерированное число от 10 до 100.
+                    array[i, j] = rnd.Next(10, 100);
+                }
+            }
+            // Возвращаем заполненный данными двумерный массив. 
+            return array; 
+        }
+    }
+}
---
+++ HomeTask_3 (№2)/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+/*
+ *  Написать программу «Телефонный справочник»: создать двумерный массив 5х2, 
+ *  хранящий список телефонных контактов: первый элемент хранит имя контакта, 
+ *  второй — номер телефона/email.
+ */
+
+namespace HomeTask_3___2_
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            string[,] telDirectory = { { "Иванов Иван", "8-988-233-54-32 | cat@mail.ru" },
+                                       { "Федоров Арсений", "8-922-278-23-01 | dog@mail.ru" },
+                                       { "Копанцев Дмитрий", "8-973-434-13-88 | pig@mail.ru" },
+                                       { "Чугунов Дмитрий", "8-901-879-37-15| | lion@mail.ru" },
+                                       { "Новиков Алексей", "8-927-534-24-67 | horse@mail.ru" } };
+                                                                                               
+            Console.WriteLine("\t\t ТЕЛЕФОННЫЙ СПРАВОЧНИК");
+            Print(telDirectory);
+            Console.Read(); 
+        }
+        static void Print(string[,] telDirectory)
+        {
+            for(int i = 0; i < 5; i++)
+            {
+                int j = 0; 
+                Console.WriteLine($"Name: {telDirectory[i, j]} -> Contacts: {telDirectory[i, ++j]}"); 
+            }
+        }
+
+    }
+}
