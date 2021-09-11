f65274a8978348f08373c2fed4d025f946b96316
ArtemKuznetsov44 <kuzia.artem456485@mail.ru>
2021-09-11 16:02:35 +0300

    HomeWork for lesson№5

---
+++ HomeTask_5 (№1)/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.IO; 
+/*
+ * Ввести с клавиатуры произвольный набор данных и сохранить его в текстовый файл.
+ */
+
+namespace HomeTask_5___1_
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Создадим новую директорию, если таковая отсутствует.
+            if (!Directory.Exists(@"..\..\..\NewDir"))
+                Directory.CreateDirectory(@"..\..\..\NewDir");
+            // Сохраним путь к файлу в строковую переменную для удобства.
+            string path = @"..\..\..\NewDir\SomeData.txt"; 
+            Console.Write("Введите произвольный набор данных: ");
+
+            string someDate = Console.ReadLine(); // Сохранение данных,
+                                                  // которые ввел пользователь в строковую переменную.
+            // Вызов функции.
+            KeepInFile(someDate, path);
+            Console.Read(); 
+        }
+        // Данная функции отвечает за запись в текстовый файл данных, которые ввел пользователь.
+        static void KeepInFile(string data, string path)
+        {
+            File.WriteAllText(path, data);
+            Console.WriteLine("Данные были записаны в файл."); 
+        }
+    }
+}
---
+++ HomeTask_5 (№2)/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.IO;
+
+
+/*
+ * Написать программу, которая при старте дописывает текущее время в файл «startup.txt».
+ */
+namespace HomeTask_5___2_
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Создадим новую директорию, если таковая отсутствует.
+            if (!Directory.Exists(@"..\..\..\NewDir"))
+                Directory.CreateDirectory(@"..\..\..\NewDir");
+            // Сохраним путь к файлу в строковую переменную для удосбства.
+            string path = @"..\..\..\NewDir\startup.txt";
+            // Вызов функции.
+            AppEndDate(path);
+        }
+        // Данная функция дописыват в файл текущее время.
+        static void AppEndDate(string path)
+        {
+            // Добавим некоторое содержимое в файл.
+            File.WriteAllText(path, "Hello! Now: ");
+            // Вывод изначального содержимого файла на консоль.
+            Console.WriteLine($"Изначальное содержимое файла: {File.ReadAllText(path)}");
+            // Дописывание в конец файла текущего времени.
+            File.AppendAllText(path, DateTime.Now.ToString("T"));
+            // Вывод итогового содержиммого на консоль.
+            Console.WriteLine($"Итоговое содержимое файла: {File.ReadAllText(path)}"); 
+
+        }
+    }
+}
