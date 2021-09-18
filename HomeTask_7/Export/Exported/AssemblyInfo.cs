05f9ce8d845fa19765c140138019aa43f8d8f7bf
ArtemKuznetsov44 <kuzia.artem456485@mail.ru>
2021-09-18 18:34:27 +0300

    Homework for lesson-7

---
+++ HomeTask_7/Export/Exported/AssemblyInfo.cs
@@ -0,0 +1,9 @@
+﻿using System.Reflection;
+
+[assembly: AssemblyCompany("HomeTask_7")]
+[assembly: AssemblyConfiguration("Release")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: AssemblyInformationalVersion("1.0.0")]
+[assembly: AssemblyProduct("HomeTask_7")]
+[assembly: AssemblyTitle("HomeTask_7")]
+[assembly: AssemblyVersion("1.0.0.0")]
